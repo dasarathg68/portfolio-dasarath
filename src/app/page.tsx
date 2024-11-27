91c9100b6809a4338a5cc025b612d37d74fcafd2
Dasarath G <dasarathg68@gmail.com>
2024-11-27 13:44:19 +0530

    feat: persist user choice

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
 import { useScroll, useSpring } from "framer-motion";
-import { useTheme } from "next-themes";
 import Navbar from "@/components/layout/NavBar";
 import Footer from "@/components/layout/Footer";
 import HeroSection from "@/components/sections/HeroSection";
@@ -19,11 +17,6 @@ import { BlockchainNetworkBackground } from "@/components/BlockchainNetworkBackg
 
 export default function Portfolio() {
   const { scrollYProgress } = useScroll();
-  const { setTheme } = useTheme();
-
-  useEffect(() => {
-    setTheme("dark");
-  }, [setTheme]);
 
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
